7b833da921ca005e05e75d04621a96f461f0cc17
omerha <haimomer@gmail.com>
2017-07-20 11:16:35 +0300

    now running demo 2

---
+++ v2/demo-app/app.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var redis = require('redis');
+
+var app = express();
+
+var redis_port = process.env.REDIS_PORT || 6379;
+var redis_host = process.env.REDIS_HOST || 'localhost';
+
+var client = redis.createClient(redis_port, redis_host);
+
+client.on('error', function (err) {
+    console.log('Client ' + err);
+});
+
+app.get('/', function (req, res, next) {
+    client.incr('counter', function(err, counter) {
+        if(err) {
+            res.status(500).send('GET ' + err);
+            return next(err);
+        }
+        res.send('Hello docker swarm meetup v2 This page has been viewed ' + counter + ' tiems' );
+        // Example for error code that swarm deploy will rollback
+        // res.send('Hello docker swarm meetup v2 \nThis page has been viewed ' + counter ' tiems' )
+        console.log(counter + ' times');
+    });
+});
+
+app.get('/healthcheck', function (req, res) {
+  res.send('up');
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+});
